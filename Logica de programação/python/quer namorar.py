e9e64841cb156d11ddf6f836328c122536d4e8bb
Marcelo Santiago De Oliveira <53163824+MrMaicke@users.noreply.github.com>
2024-12-05 19:09:48 -0300

    teste

--- Logica de programação/python/quer namorar.py
+++ Logica de programação/python/quer namorar.py
@@ -1,8 +1,14 @@
 import webbrowser
 
+# Loop infinito até receber a resposta correta
 while True:
     res = input("Quer namorar comigo? : ")
+    # Verifica se a resposta está na lista de variações aceitáveis
     if res.lower() in ["sim", "s", "claro", "com certeza", "simmm", "siim", "siiim", "siiimmm", "sii", "yes"]:
         print("Oba! Agora estamos namorando! :)")
-        webbrowser.open("https://youtu.be/dQw4w9WgXcQ?si=uwW03Ls_bmh5EGsm")
+        # Tenta abrir o link no navegador
+        try:
+            webbrowser.open("https://youtu.be/dQw4w9WgXcQ?si=uwW03Ls_bmh5EGsm")
+        except Exception as e:
+            print(f"Erro ao tentar abrir o link: {e}")
         break
