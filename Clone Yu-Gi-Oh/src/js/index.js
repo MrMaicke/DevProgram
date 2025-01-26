641d18ad1d5c6e73981ab53aeb724d4a3625e3f9
Marcelo Santiago De Oliveira <53163824+MrMaicke@users.noreply.github.com>
2025-01-26 19:02:24 -0300

    js

--- Clone Yu-Gi-Oh/src/js/index.js
+++ Clone Yu-Gi-Oh/src/js/index.js
@@ -12,25 +12,18 @@ OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão ante
   - passo 4 - buscar o cartão que esta selecionado e esconder
 */
 
-/*OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo 
-cartao da lista
-console.log(document.getElementById("btn-avancar"));****
- passo 1 - dar um jeito de pegar o elemento HTML da seta avancar*/
 const btnAvancar = document.getElementById("btn-avancar");//agora posso usar o btn-avancar no js
+const btnVoltar = document.getElementById("btn-voltar")
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
-//passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
 btnAvancar.addEventListener("click", function () {    
     if(cartaoAtual === cartoes.length -1) return;
 
-//passo 4 - buscar o cartão que esta selecionado e esconder
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
+    esconderCartaoSelecionado();
 
-//passo 3 - fazer aparecer o próximo cartão da lista
     cartaoAtual++;
-    cartoes[cartaoAtual].classList.add("selecionado");
+    mostrarCartao();
 });
 
 /*
@@ -41,16 +34,21 @@ OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão ante
   - passo 4 - buscar o cartão que esta selecionado e esconder
 */
 
-const btnVoltar = document.getElementById("btn-voltar")
 
-btnVoltar.addEventListener("click", function () {  
-    console.log(cartaoAtual);
-  
+btnVoltar.addEventListener("click", function () {    
     if(cartaoAtual === 0) return;
 
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
+    esconderCartaoSelecionado();
 
     cartaoAtual--;
-    cartoes[cartaoAtual].classList.add("selecionado");
+    mostrarCartao();
 });
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
