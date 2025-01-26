b3fe9db399fe63e7d74ae0a17b472dfcf81bd708
Marcelo Santiago De Oliveira <53163824+MrMaicke@users.noreply.github.com>
2025-01-26 19:06:17 -0300

    refaturado

--- Clone Yu-Gi-Oh/src/js/index.js
+++ Clone Yu-Gi-Oh/src/js/index.js
@@ -26,15 +26,6 @@ btnAvancar.addEventListener("click", function () {
     mostrarCartao();
 });
 
-/*
-OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão anterior da lista
-  - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
-  - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
-  - passo 3 - fazer aparecer o cartão anterior da lista
-  - passo 4 - buscar o cartão que esta selecionado e esconder
-*/
-
-
 btnVoltar.addEventListener("click", function () {    
     if(cartaoAtual === 0) return;
 
