25e981227947a454fe1ff8f371e5fd7f8bb9982d
Marcelo Santiago De Oliveira <53163824+MrMaicke@users.noreply.github.com>
2025-01-26 18:51:13 -0300

    voltar e avançar

--- Clone Yu-Gi-Oh/src/js/index.js
+++ Clone Yu-Gi-Oh/src/js/index.js
@@ -18,10 +18,39 @@ console.log(document.getElementById("btn-avancar"));****
  passo 1 - dar um jeito de pegar o elemento HTML da seta avancar*/
 const btnAvancar = document.getElementById("btn-avancar");//agora posso usar o btn-avancar no js
 const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
 
 //passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
-btnAvancar.addEventListener("click", function () {
+btnAvancar.addEventListener("click", function () {    
+    if(cartaoAtual === cartoes.length -1) return;
+
+//passo 4 - buscar o cartão que esta selecionado e esconder
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
 
 //passo 3 - fazer aparecer o próximo cartão da lista
-    cartoes[1].classList.add("");
+    cartaoAtual++;
+    cartoes[cartaoAtual].classList.add("selecionado");
+});
+
+/*
+OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão anterior da lista
+  - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+  - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
+  - passo 3 - fazer aparecer o cartão anterior da lista
+  - passo 4 - buscar o cartão que esta selecionado e esconder
+*/
+
+const btnVoltar = document.getElementById("btn-voltar")
+
+btnVoltar.addEventListener("click", function () {  
+    console.log(cartaoAtual);
+  
+    if(cartaoAtual === 0) return;
+
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+
+    cartaoAtual--;
+    cartoes[cartaoAtual].classList.add("selecionado");
 });
